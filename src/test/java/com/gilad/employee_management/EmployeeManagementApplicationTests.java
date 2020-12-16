dd52b434ac8f4a41bbf4138a28b4866f53a00130
Gilad <abudig@post.bgu.ac.il>
2020-12-16 16:59:51 +0200

    Spring boot init

---
+++ src/test/java/com/gilad/employee_management/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilad.employee_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
